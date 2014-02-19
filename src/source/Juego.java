84d08c5d58d38cdb70b701b591f4243e5d50ef26
ppesq <betoesquivel>
2014-02-19 13:45:32 -0600

    first commit

---
+++ src/source/Juego.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package source;
+
+/**
+ *
+ * @author ppesq
+ */
+public class Juego {
+
+    public static void main(String[] args) {
+        JFrameJuego1 miJuego = new JFrameJuego1();
+    }
+}
